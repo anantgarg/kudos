408ae95989c217343ef3aa64eae263a8afbc4982
Anant Garg <anant.garg@inscripts.com>
2015-08-11 12:38:10 +0530

    Installation instructions

--- config.default.php
+++ config.default.php
@@ -1,10 +1,15 @@
 <?php
 
+// Database details
+
 define('SERVERNAME','localhost');
 define('SERVERPORT','3306');
 define('DBUSERNAME','');
 define('DBPASSWORD','');
 define('DBNAME','');
+
+// URL to Kudos with trailing slash
+
 define('BASE_URL','http://localhost/kudos/');
 
 define('FB_APPID','');
---
+++ install.md
@@ -0,0 +1,6 @@
+# Installation instructions
+
+1. Rename config.default.php to config.php
+2. Create a new database
+3. Edit config.php and add the necessary details
+4. Run yoursite.com/kudos/install
