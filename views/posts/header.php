e67355a2f0f2309c5832c77536fa483f64d1c979
Anant Garg <anant.garg@inscripts.com>
2015-08-11 14:05:35 +0530

    Fix

--- views/posts/header.php
+++ views/posts/header.php
@@ -4,7 +4,7 @@
 			<div class="list-group">
 			  	<?php foreach ($accounts as $account):?>
 
-				<a class="list-group-item <?php if (!empty($account['current'])):?>active<?php endif;?>" href="<?php echo BASE_URL;?>posts/live/<?php echo $account['id'];?>"><?php echo $account['name'];?> (<?php echo ucwords($account['type']);?>)</a>
+				<a class="list-group-item <?php if (!empty($account['current'])):?>active<?php endif;?>" href="<?php echo BASE_URL;?>posts/live/<?php echo $account['id'];?>"><img src="<?php echo $account['image'];?>" class="nav-image"><?php echo $account['name'];?> (<?php echo ucwords($account['type']);?>)</a>
 
 				<?php endforeach;?>
 			</div>
